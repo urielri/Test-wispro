2b51a897a8ed06332577f8a049e63945b4500210
urielri <urielrivps@gmail.com>
2021-05-12 12:19:06 -0300

    deploy

--- components/utils/architecture/table/item.tsx
+++ components/utils/architecture/table/item.tsx
@@ -1,6 +1,10 @@
 import styles from "./style.module.scss";
 import { UserInfo } from "components/interface";
-function Item(props: { designation: string; data: UserInfo; handleModal: Function }): JSX.Element {
+function Item(props: {
+  designation: string;
+  data: UserInfo;
+  handleModal: Function;
+}): JSX.Element {
   const { designation, data, handleModal } = props;
   return (
     <div
@@ -8,24 +12,24 @@ function Item(props: { designation: string; data: UserInfo; handleModal: Functio
         designation == "identifiers"
           ? `${styles.row} ${styles.identifiers}`
           : `${styles.row} ${styles.field}`
-      } onClick={handleModal}
+      }
+      onClick={() => handleModal()}
     >
       {designation == "identifiers"
-        ? Object.keys(data).map((res, index) => <Field isIdentifier name={res} key={index} />)
-        : Object.values(data).map((res, index) => <Field name={res}  key={index}/>)}
+        ? Object.keys(data).map((res, index) => (
+            <Field isIdentifier name={res} key={index} />
+          ))
+        : Object.values(data).map((res, index) => (
+            <Field name={res} key={index} />
+          ))}
     </div>
   );
 }
-function Field(props: { name: string, isIdentifier?: boolean }): JSX.Element {
+function Field(props: { name: string; isIdentifier?: boolean }): JSX.Element {
   const { name, isIdentifier } = props;
   return (
     <div className={styles.info}>
-      {
-        isIdentifier ?
-        <h4>{name}</h4>
-        :
-        <span>{name}</span>
-      }
+      {isIdentifier ? <h4>{name}</h4> : <span>{name}</span>}
     </div>
   );
 }
